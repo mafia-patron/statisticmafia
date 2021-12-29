a9edec0e7ddd327691994e2136b1ab7d02d46e76
Mafia-Neft-Club <94564571+Mafia-Neft-Club@users.noreply.github.com>
2021-12-29 23:03:17 +0300

    Update app.87223b4c.js

--- js/app.87223b4c.js
+++ js/app.87223b4c.js
@@ -945,7 +945,7 @@
                     redWinsCount : 0,
                     blackWinsCount : 0,
                     gamesForRatingCount : 0,
-                    ratingThreshold : .14,
+                    ratingThreshold : 14,
                     currentSort : "name",
                     currentSortOrder : "asc",
                     database : null,
